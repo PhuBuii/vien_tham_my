6ea744a202d149574ed17933b4bfc5d21727ab96
PhuBuii <buiquangphu.work@gmail.com>
2025-05-07 13:45:27 +0700

    Reduce image size

--- assets/js/trangchu.js
+++ assets/js/trangchu.js
@@ -1,9 +1,20 @@
-new Swiper(".swiper", {
+new Swiper("#feedback-swiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+});
+new Swiper("#hero-swiper", {
+  loop: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+  navigation: {
+    nextEl: ".hero-button-next",
+    prevEl: ".hero-button-prev",
   },
 });
